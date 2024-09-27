b47de37f41361126dbb832cd1093cab09d1feed3
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-09-27 14:03:26 -0400

    add data folder to clean up structure and configure start of BentoGrid

---
+++ data/index.ts
@@ -0,0 +1,6 @@
+export const gridItems = [
+  { title: 'Title 1', description: 'Description 1', id: 1 },
+  { title: 'Title 1', description: 'Description 1', id: 2 },
+  { title: 'Title 1', description: 'Description 1', id: 3 },
+  { title: 'Title 1', description: 'Description 1', id: 4 }
+]
