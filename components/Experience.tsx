5ad211e915a8046ee5618481f7fea3f3e71f347a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-09-30 10:55:00 -0400

    fix styling issues on projects and experience sections

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -4,12 +4,12 @@ import { Button } from "./ui/MovingBorders";
 
 const Experience = () => {
   return (
-    <div className="py-20" id="testimonials">
+    <div className="py-20 w-full" id="experience">
       <h1 className="heading">
         My
         <span className="text-purple"> work experience</span>
       </h1>
-      <div className="w-full mt-12 grid lg:grid-col-4 grid-cols-1 gap-10">
+      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
         {workExperience.map((card) => (
           <Button
             key={card.id}
