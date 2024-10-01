60659997be4e96827e1ffc9ebdc2423803539642
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-10-01 08:13:48 -0400

    change line-clamp on desc of projects

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -26,7 +26,7 @@ const RecentProjects = () => {
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
                 {title}
               </h1>
-              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+              <p className="lg:text-xl lg:font-sm font-light text-sm line-clamp-8 mt-2">
                 {des}
               </p>
               <div className="flex items-center justify-between mt-7 mb-3">
